a477a08f93e86d7d4ab78dc3f0ea060be757db4a
Rodrigo Luiz Duarte <rodrigo.luizduarte@gmail.com>
2017-12-08 19:35:47 -0200

    Implemetada a geração de número secreto aleatório.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,14 +1,18 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+valor_minimo_chute = 1
+valor_maximo_chute = 100
+
+for rodada in range(1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre {} e {}: ".format(1,100))
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
@@ -16,15 +20,17 @@ while(rodada <= total_de_tentativas):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
+    if (chute < valor_minimo_chute or chute > valor_maximo_chute):
+        print("Você deve informar um número entre {} e {}".format(1,100))
+        continue
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
-
-    rodada = rodada + 1
 
 print("Fim do jogo")
